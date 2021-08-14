bce370a9c0a1da0546e95df1cc7b0a6ca43e3ecd
enesbayrktar <enes.bayraktar@outlook.com.tr>
2021-08-14 10:07:40 +0300

    Extend font-color to body

--- src/pages/certificates.js
+++ src/pages/certificates.js
@@ -6,6 +6,7 @@ function Certificates() {
     return (
         <Wrapper size="md" className="px-4 mt-3">
             <PageTransition>
+                <p className="text-highlight text-2xl">test</p>
                 <Social color="green">Test</Social>
             </PageTransition>
         </Wrapper>
