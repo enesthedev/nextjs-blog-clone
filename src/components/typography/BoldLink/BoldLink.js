63ed7c99aa84c4a002b8cb9feb59b291d9b05adc
enesbayrktar <enes.bayraktar@outlook.com.tr>
2021-08-16 09:13:19 +0300

    Add Next/Link support

--- src/components/typography/BoldLink/BoldLink.js
+++ src/components/typography/BoldLink/BoldLink.js
@@ -1,22 +1,33 @@
 import PropTypes from "prop-types"
+import Link from "next/link"
 
 function BoldLink(props) {
-    const { highlight, content, to, className } = props
-    return (
-        <a className={`font-bold hover:underline hover:text-${highlight} ${className}`} target="_blank" href={to} rel="noreferrer">
-            { content }
-        </a>
+    const { highlight, content, to, router, className } = props
+    return ((router === true) ?
+        <Link href={to}>
+            <a className={`font-bold hover:underline hover:text-${highlight} ${className}`}>
+                { content }
+            </a>
+        </Link>
+        :
+            <>
+                <a className={`font-bold hover:underline hover:text-${highlight} ${className}`} target="_blank" href={to} rel="noreferrer">
+                    { content }
+                </a>
+            </>
     )
 }
 
 BoldLink.propTypes = {
     content: PropTypes.string.isRequired,
     to: PropTypes.string.isRequired,
-    highlight: PropTypes.string
+    highlight: PropTypes.string,
+    router: PropTypes.bool
 }
 
 BoldLink.defaultProps = {
-    highlight: "yellow-400"
+    highlight: "yellow-400",
+    router: false
 }
 
 export default BoldLink
