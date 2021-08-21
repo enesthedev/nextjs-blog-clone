887f86ac08c5d64ba1bbda483be2f17a51093522
enesbayrktar <enes.bayraktar@outlook.com.tr>
2021-08-21 19:58:12 +0100

    Add some social-media icons svg from font-awesome

--- src/components/icons/index.js
+++ src/components/icons/index.js
@@ -1,3 +1,11 @@
 import MenuIcon from "./MenuIcon/MenuIcon"
+import FacebookIcon from "./FacebookIcon/FacebookIcon"
+import TwitterIcon from "./TwitterIcon/TwitterIcon"
+import LinkedInIcon from "./LinkedInIcon/LinkedInIcon"
 
-export { MenuIcon }
+export {
+    MenuIcon,
+    FacebookIcon,
+    TwitterIcon,
+    LinkedInIcon
+}
