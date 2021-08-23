ba7b41259165af0be13680c3c59db60c979ba2b1
enesbayrktar <enes.bayraktar@outlook.com.tr>
2021-08-23 15:23:13 +0100

    Refactor index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,46 +5,105 @@ import { Hero, Social, BoldLink, Divider } from "components/typography"
 import { Wrapper } from "components/layout"
 import { NextSeo } from "next-seo"
 
-function Index() {
+function Index(props) {
+    const { page } = props
     return (
         <React.Fragment>
             <NextSeo
-                title="Anasayfa"
+                title={page.head.title}
+                description={page.head.description}
             />
-            <Wrapper size="md" className="px-4">
+            <Wrapper size="md" className="px-4 pt-8 pb-2">
                 <PageTransition>
-                    <Hero title={{
-                        text: `
-                        Ben Enes Bayraktar.
-                        İstanbulda Üniversite okuyan bir Yazılımcıyım.
-                    `
-                    }} className="pb-7">
-                        <p>Javascript, front-end teknolojileri, web servisleri, bilgisayar oyunları ve araştırma gibi konularla yakından ilgileniyorum.</p>
-                        <p>Yaptığım yazılımlar ile alakalı kaynak kodları açık bir şekilde <BoldLink to="https://github.com/enesbayrktar/" content="Github" /> adresimde paylaşıyorum.</p>
-                        <p>İnsanların başarılı olabilmek için sadece yeteneğe değil çok çalışmaya ihtiyaçları olduğuna inanıyorum.</p>
+                    <Hero title={{ text: page.title }} className="pb-7">
+                        {typeof page.description === "object" ? page.description.map((text, index) => {
+                            return (
+                                <p key={index}>
+                                    { text }
+                                </p>
+                            )
+                        }) : page.description}
                     </Hero>
-                    <Divider color="white" opacity="20"/>
-                    <Hero title={{
-                        text: "Hakkımda daha fazlası",
-                        size: "lg"
-                    }} gap={"5"} className="mt-9 pb-5">
-                        <p className="text-sm whitespace-pre-line">
-                            Yazılım dillerine olan ilgim 14 yaşımdayken başladı. O zamanlar sınıfta dışlanan çocuk olmamın da etkisi var tabii ki.
-                            Abimin yeni aldığı Asus marka bilgisayar üzerinde <BoldLink to="https://mtasa.com/" content="GTA: San Andreas Online" /> oynuyordum her akşam. Bahsettiğim oyun öyle bir sistem ki istediğiniz oyun modunda
-                            bir şeyler karalayabiliyor, kendinize eğlence çıkarabiliyorsunuz... Herneyse, günlerden bir gün ben de tutuldum bir sunucu açacağım diye.
-                        </p>
-                        <p className="text-sm whitespace-pre-line">
-                            Araştırmalara başladım tabii içimde bir heyecan. İndirdim hazır bir sunucu ve açtım oyunu. İlk defa kendi sunucumu oluşturmuştum ki sol üstte
-                            'Blah blah Gaming' yazmaktaydı. Garip olucak ama onu değiştirmek için dosyalarda aranırken buldum kendimi. Böylece ilk Merhaba Dünyamı demiş oldum bu sektöre
-                        </p>
-                        <Social color="yellow" size="sm" top="0">
-                            İşte o gün bugündür kodlarla iç içe bir hayat sürüyorum. Bundan oldukça memnunum ve yaptığım işe aşığım. Bence en önemlisi de bu nokta..
-                        </Social>
-                    </Hero>
+                </PageTransition>
+            </Wrapper>
+            <Wrapper size="lg" className="px-4 pb-4 flex justify-center align-center">
+                <PageTransition>
+                    <div className="flex flex-col gap-7 px-4 max-w-screen-md xl:max-w-screen-lg xl:flex-row">
+                        <Hero title={{ text: page.sections.head.title, size: "base" }} gap={"5"} className="mt-9  xl:p-12 xl:border-2 xl:border-r-0">
+                            {typeof page.sections.head.description === "object" ? page.sections.head.description.map((description, index) => {
+                                return (
+                                    <p key={index} className="text-sm whitespace-pre-line">
+                                        { description }
+                                    </p>
+                                )
+                            }) : page.sections.head.description}
+                            {page.sections.head.socials ? page.sections.head.socials.map((social, index) => {
+                                return (
+                                    <Social color={social.color} size="sm" top="0">
+                                        { social.text }
+                                    </Social>
+                                )
+                            }) : ''}
+                        </Hero>
+                        <Hero title={{ text: page.sections.sub.title, size: "base" }} gap={"5"} className="mt-9 xl:p-12 xl:border-2 xl:border-l-8 xl:border-white xl:border-opacity-10 xl:mt-0">
+                            {typeof page.sections.sub.description === "object" ? page.sections.sub.description.map((description, index) => {
+                                return (
+                                    <p key={index} className="text-sm whitespace-pre-line">
+                                        { description }
+                                    </p>
+                                )
+                            }) : page.sections.sub.description}
+                            {page.sections.sub.socials ? page.sections.sub.socials.map((social, index) => {
+                                return (
+                                    <Social color={social.color} size="sm" top="0">
+                                        { social.text }
+                                    </Social>
+                                )
+                            }) : ''}
+                        </Hero>
+                    </div>
                 </PageTransition>
             </Wrapper>
         </React.Fragment>
     )
+}
+
+export async function getStaticProps() {
+    return {
+        props: {
+            page: {
+                title: "Ben Enes Bayraktar.\nİstanbulda Üniversite okuyan bir Yazılımcıyım.",
+                description: [
+                    "Javascript, front-end teknolojileri, web servisleri, bilgisayar oyunları ve araştırma gibi konularla yakından ilgileniyorum.",
+                    "Yaptığım yazılımlar ile alakalı kaynak kodları açık bir şekilde Github adresimde paylaşıyorum.",
+                    "İnsanların başarılı olabilmek için sadece yeteneğe değil çok çalışmaya ihtiyaçları olduğuna inanıyorum."
+                ],
+                head: {
+                    title: "Giriş",
+                    description: "Enes Bayraktar'ın kişisel bloğunda hakkında yayınlamış olduğu iletişim bölümü. Buradan kendisine ulaşabilirsiniz."
+                },
+                sections: {
+                    head: {
+                        title: "Kimim ben?",
+                        description: [
+                            "ismim Enes. 25 Eylül 2000 tarihinde Trabzonda bir devlet hastanesinde doğdum. Küçük yaşlarda abilerim sayesinde bilgisayarla tanıştım.",
+                            "İlk bilgisayarım toplama bir kasaydı. Yıkanılabilir topu olan bir mouse ve kocaman tuşları olan bir klavye ile merhaba dedim elektronik dünyaya."
+                        ],
+                        socials: false
+                    },
+                    sub: {
+                        title: "Yazılım kariyerim nasıl başladı?",
+                        description: [
+                            "Oynadığım oyunlarda kurulan klanlara üye olmak için tıkladığım linklerle başladı websiteleri ile olan etkileşimim. O zamanlar oyunlarda klan üyelerinden ayrışabilmek için Grafiker, Kodlamacı vesaire gibi bir sürü kavram vardı.",
+                            "Bende kolları sıvadım açtım Photoshop'u başladım tasarlamalaya. Bir şeyler yaptıkca ve yaptıklarım insanlar tarafından beğenildikçe içime işledi bu duygu.",
+                            "Tasarımlarımı ilk MyBB forum sistemine yapmıştım ancak kodlayabilecek insan olmayınca araştırmalar vesaire derken HTML, CSS ve Javascrpt üçlüsüyle tanıştım. Bu süreçte tanıştığım insanlar hem yaşımın küçüklüğünden hem de yetenek mi gördüler bilmiyorum ancak bana çok yardımcı olmuşlardı.",
+                            "Bu heves ile beraber farklı internet siteleri ve projeler yapma yolunda ilerlemeye başladım. İşte şimdi olduğum yere geldim. 2014'de başlayan bu serüven benim geleceğim oldu."
+                        ]
+                    }
+                }
+            }
+        }
+    }
 }
 
 export default Index
