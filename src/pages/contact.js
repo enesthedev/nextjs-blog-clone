403e89c1bd8eabe4b21e44bd52e9115a5a0c3a10
enesbayrktar <enes.bayraktar@outlook.com.tr>
2021-08-23 13:16:05 +0100

    Convert contact page to half dynamic

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,69 +1,79 @@
 import React from "react"
 
 import { PageTransition } from "components/transition"
-import { Divider, Hero } from "components/typography"
+import { Divider, Hero, Social } from "components/typography"
 import { Wrapper } from "components/layout"
-import { FacebookIcon, TwitterIcon } from "components/icons";
 import { NextSeo } from "next-seo"
 
-function Contact() {
+function Contact(props) {
+    const { page, social } = props
     return (
         <React.Fragment>
             <NextSeo
-                title="İletişim"
+                title={page.head.title}
+                description={page.head.description}
             />
-            <Wrapper className="px-4">
+            <Wrapper className="px-4 pt-8 pb-2">
                 <PageTransition>
-                    <Hero title={{
-                        text: `
-                        Sosyal medya hesaplarım üzerinden ya da burada dolduracağınız form üzerinden benimle iletişime geçebilirsiniz.
-                        `,
-                        weight: "base"
-                    }}>
-                        <div className="flex flex-col gap-6 mt-8">
+                    <Hero title={{ text: page.title, weight: "base" }}>
+                        <small>{ page.description }</small>
+                        <Social color="green" size="sm" top="0" link={`https://wa.me/${social.whatsapp.phone}?text=${social.whatsapp.message}`}>Benimle hemen <b>Whatsapp</b> üzerinden konuşmak için tıklayın ⟶</Social>
+                        <div className="flex flex-col gap-8">
+                            <Divider color="white" opacity="10"/>
                             <form autoComplete="false" className="flex flex-col gap-6">
-                                <div className="flex flex-col gap-7 sm:flex-row">
-                                    <label className="block w-full">
-                                        <span className="text-sm">Adınız ve Soyadınız</span>
-                                        <input type="text" className="mt-1 block w-full px-0.5 pl-2 bg-transparent border border-opacity-10 focus:ring-0 focus:border-white focus:border-opacity-20" />
-                                    </label>
-                                    <label className="block w-full">
-                                        <span className="text-sm">E-posta adresiniz</span>
-                                        <input type="text" className="mt-1 block w-full px-0.5 bg-transparent border-0 border-b border-white border-opacity-30 focus:ring-0 focus:border-white focus:border-opacity-20" />
-                                    </label>
-                                </div>
-                                <div className="flex flex-col gap-7">
-                                    <label className="block w-full">
-                                        <span className="text-sm">Mesajınız</span>
-                                        <textarea className="mt-1 block w-full px-0.5 pl-2 bg-transparent border border-opacity-10 h-32 focus:ring-0 focus:border-white focus:border-opacity-20" />
-                                    </label>
-                                    <button className="text-base hover:underline">Gönder</button>
+                                <label className="block w-full">
+                                    <span className="text-sm flex flex-col gap-1">
+                                        <div>{ page.form.label.title } <small className="font-bold text-sm text-red-400">*</small></div>
+                                        <small className="text-xs mt-2 mb-2">{ page.form.label.description }</small>
+                                    </span>
+                                    <input type="email" placeholder="yourname@mail.com" required className="mt-3 block w-full px-0.5 pl-2 bg-transparent border-2 border-white border-opacity-10 placeholder-white placeholder-opacity-20 focus:ring-0 focus:border-opacity-20" />
+                                </label>
+                                <div className="text-right text-base">
+                                    <button className="hover:underline">{ page.form.button.title }</button>
                                 </div>
                             </form>
-                            <div className="flex gap-5 flex-col">
-                                <Divider color="white" opacity="10"/>
-                                <div className="flex gap-4 flex-col">
-                                    <Hero title={{
-                                        text: "Lütfen görmek istediğiniz sosyal medya platformuna tıklayınız",
-                                        size: "sm",
-                                        weight: "base"
-                                    }} />
-                                    <div className="flex gap-6 flex-row">
-                                        <a href="https://web.facebook.com/enes.bayraktar.7334/" target="_blank" className="transition transition-all duration-100 hover:text-blue-500">
-                                            <FacebookIcon size={24}/>
-                                        </a>
-                                        <a href="https://twitter.com/enesthedev" target="_blank" className="transition transition-all duration-100 hover:text-blue-400">
-                                            <TwitterIcon size={24}/>
-                                        </a>
-                                    </div>
-                                </div>
-                            </div>
                         </div>
                     </Hero>
                 </PageTransition>
             </Wrapper>
         </React.Fragment>
     )
+}
+
+export async function getStaticProps() {
+    /**
+     * Static fields;
+     * These fields cannot change with this props. İf you want to change you can change it by manually.
+     *
+     * @static <Social color="green"... at line 20
+     */
+    return {
+        props: {
+            page: {
+                title: "Bu sayfada benimle iletişime geçebilmek için gereken tüm bilgileri bulabilirsiniz.",
+                description: "Sizlerle beraber güzel projelere adım atalabileceğimize inanıyorum. Benimle iletişim halinde bulunmak için altta verilmiş olan formu doldurmanız yeterlidir. Ben size en yakın zamanda döneceğim.",
+                head: {
+                    title: "İletişim",
+                    description: "Enes Bayraktar'ın kişisel bloğunda hakkında yayınlamış olduğu iletişim bölümü. Buradan kendisine ulaşabilirsiniz."
+                },
+                form: {
+                    label: {
+                        title: "E-posta Adresiniz",
+                        description: "Bu formu gönderdiğinizde Kişisel Verilerin Korunması Kanunu uyarınca e-posta adresinizi benimle paylaşmış olursunuz. İlerlemek için \"Benimle iletişime geç\" yazısına tıklayın."
+                    },
+                    button: {
+                        title: "Benimle iletişime geç"
+                    }
+                }
+            },
+            social: {
+                whatsapp: {
+                    phone: "905340631477",
+                    message: "Sizinle%20en%20yakın%20zamanda%20görüşmek%20istiyorum.",
+                }
+            }
+        }
+    }
 }
 
 export default Contact
