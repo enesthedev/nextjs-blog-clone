4360617565629f2753edb98cf48edc182a596c13
enesbayrktar <enes.bayraktar@outlook.com.tr>
2021-08-25 17:59:54 +0100

    Add canonical pageProp support

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,8 +1,8 @@
 import React from "react"
 import App from "next/app"
+import Head from "next/head"
 
 import { PageLayout } from "components/layout"
-import { DefaultSeo } from "next-seo"
 
 import "styles/tailwind.css"
 
@@ -11,11 +11,14 @@ class BlogApp extends App {
         const { Component, pageProps } = this.props
         return (
             <React.Fragment>
-                <DefaultSeo
-                    titleTemplate="%s - Enes Bayraktar"
-                    dangerouslySetAllPagesToNoIndex={true}
-                    dangerouslySetAllPagesToNoFollow={true}
-                />
+                <Head>
+                    <meta name="robots" content="index, follow" />
+                    <meta httpEquiv="x-ua-compatible" content="ie=edge" />
+                    <meta name="author" content="Enes Bayraktar <bilgi@enesbayraktar.com.tr>" />
+                    <meta name="keywords" content="Enes Bayraktar, enes, blog, portfolio, iletişim, cv, geliştirici, front end developer, senior developer," />
+                    <meta name="application-name" content="Enes Bayraktar'ın Bloğu" />
+                    {pageProps.canonical ? <link rel="canonical" href={pageProps.canonical} /> : ""}
+                </Head>
                 <PageLayout menus={{
                     '/': 'Giriş',
                     '/resume': 'Özgeçmiş',
