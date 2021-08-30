99da21d1eaaa24d3e3e9ed024ad4e1cd5a3266d6
enesbayrktar <enes.bayraktar@outlook.com.tr>
2021-08-30 12:12:04 +0100

    Change _app structure class to function component

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,35 +1,32 @@
 import React from "react"
-import App from "next/app"
 import Head from "next/head"
 
 import { PageLayout } from "components/layout"
 
 import "styles/tailwind.css"
 
-class BlogApp extends App {
-    render() {
-        const { Component, pageProps } = this.props
-        return (
-            <React.Fragment>
-                <Head>
-                    <meta name="robots" content="index, follow" />
-                    <meta httpEquiv="x-ua-compatible" content="ie=edge" />
-                    <meta name="author" content="Enes Bayraktar <bilgi@enesbayraktar.com.tr>" />
-                    <meta name="keywords" content="Enes Bayraktar, enes, blog, portfolio, iletişim, cv, geliştirici, front end developer, senior developer," />
-                    <meta name="application-name" content="Enes Bayraktar'ın Bloğu" />
-                    {pageProps.canonical ? <link rel="canonical" href={pageProps.canonical} /> : ""}
-                    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap" rel="stylesheet" />
-                </Head>
-                <PageLayout menus={{
-                    '/': 'Giriş',
-                    '/certificates': 'Sertifikalar',
-                    '/contact': 'İletişim'
-                }}>
-                    <Component {...pageProps} />
-                </PageLayout>
-            </React.Fragment>
-        )
-    }
+function App(props) {
+    const { Component, pageProps } = props
+    return (
+        <React.Fragment>
+            <Head>
+                <meta name="robots" content="index, follow" />
+                <meta httpEquiv="x-ua-compatible" content="ie=edge" />
+                <meta name="author" content="Enes Bayraktar <bilgi@enesbayraktar.com.tr>" />
+                <meta name="keywords" content="Enes Bayraktar, enes, blog, portfolio, iletişim, cv, geliştirici, front end developer, senior developer," />
+                <meta name="application-name" content="Enes Bayraktar'ın Bloğu" />
+                { pageProps.canonical ? <link rel="canonical" href={pageProps.canonical} /> : "" }
+                <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap" rel="stylesheet" />
+            </Head>
+            <PageLayout menus={{
+                '/': 'Giriş',
+                '/certificates': 'Sertifikalar',
+                '/contact': 'İletişim'
+            }}>
+                <Component {...pageProps} />
+            </PageLayout>
+        </React.Fragment>
+    )
 }
 
-export default BlogApp
+export default App
